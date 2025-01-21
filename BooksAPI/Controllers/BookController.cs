105ef4dd96829d4cb35446ef47e1009533c19e73
Waleed <walizia200@gmail.com>
2025-01-21 05:57:22 +0500

    Add .gitignore and remove .vs folder from tracking

---
+++ BooksAPI/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(new[] { "Book1", "Book2" });
+        }
+    }
+}
